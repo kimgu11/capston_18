614f3cf9c4001bfba90010437582e11abdf62469
Yebin <ferf99@naver.com>
2023-05-27 09:42:07 +0900

    관리자 신청페이지 함수 수정

--- lib/screens/gScore/gscore_admin_regist_screen.dart
+++ lib/screens/gScore/gscore_admin_regist_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:file_picker/file_picker.dart';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'dart:io';
+
 
 //신청창
 void main() {
@@ -23,6 +23,7 @@ class GScoreAdminRegist extends StatefulWidget {
 class _GScoreAdminRegistState extends State<GScoreAdminRegist> {
   void initState() {
     super.initState();
+    _getuserInfo();
   }
 
   Future<void> _writePostAndFile() async {
@@ -62,9 +63,9 @@ class _GScoreAdminRegistState extends State<GScoreAdminRegist> {
       'gspost_category': _activityType,
       'gspost_item': _activityName,
       'gspost_content': _contentController.text,
-      'gspost_pass': _applicationStatus,
-      'gspost_reason': _rejectionReason,
-      'gspost_file': fileCheck,
+      'gspost_pass': '승인',
+      'gspost_reason': '',
+      'gspost_file': '0',
     };
 
     final response = await http.post(
@@ -80,108 +81,15 @@ class _GScoreAdminRegistState extends State<GScoreAdminRegist> {
 
     if (response.statusCode == 201) {
       postUploadCheck = 1;
-      if (fileCheck == 1) {
-        var jsonResponse = jsonDecode(response.body);
-        postId = jsonResponse['postId'];
-        //uploadFile();
-      }
+
     } else {
       print(response.statusCode);
       print('에러');
     }
   }
 
-  Future<void> uploadFile() async {
-    print(postId.toString());
 
-    if (selectedFile != null) {
-      final String fileName = selectedFile!.name;
-      final bytes = File(selectedFile!.path!).readAsBytesSync();
 
-      final maxRetries = 3; // 최대 재시도 횟수
-      var retryCount = 0; // 현재 재시도 횟수
-
-      while (retryCount < maxRetries) {
-        try {
-          final request = http.MultipartRequest(
-            'POST',
-            Uri.parse('http://3.39.88.187:3000/gScore/upload'),
-          );
-
-          request.files.add(
-            http.MultipartFile.fromBytes('file', bytes, filename: fileName),
-          );
-
-          request.fields['gspostid'] = postId.toString();
-
-          final response = await request.send();
-          print(response.statusCode);
-
-          if (response.statusCode == 201) {
-            print("파일 등록 성공");
-
-            var responseData = await response.stream.bytesToString();
-            var decodedData = json.decode(responseData);
-            var file = decodedData['file'];
-
-            fileInfo = {
-              'post_id': postId,
-              'file_name': file['filename'],
-              'file_original_name': file['originalname'],
-              'file_size': file['size'],
-              'file_path': file['path'],
-            };
-            fileUploadCheck = 1;
-
-            print(fileInfo);
-            return; // 성공적으로 요청을 보냈으면 메서드를 종료
-          } else {
-            print(response.statusCode);
-            print("파일 등록 실패");
-          }
-        } catch (error) {
-          print('등록 네트워크 연결 오류: $error');
-        }
-
-        retryCount++;
-        await Future.delayed(Duration(seconds: 1)); // 1초 후에 재시도
-      }
-
-      print('재시도 횟수 초과');
-    }
-  }
-
-  Future<void> _uploadfileToDB() async {
-    final maxRetries = 3; // 최대 재시도 횟수
-    var retryCount = 0; // 현재 재시도 횟수
-
-    while (retryCount < maxRetries) {
-      try {
-        final response = await http.post(
-          Uri.parse('http://3.39.88.187:3000/gScore/fileToDB'),
-          headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8',
-          },
-          body: jsonEncode(fileInfo),
-        );
-
-        if (response.statusCode == 201) {
-          print('DB저장 완료');
-          return; // 성공적으로 요청을 보냈으면 메서드를 종료
-        } else {
-          print(response.statusCode);
-          print('에러');
-        }
-      } catch (error) {
-        print('DB 네트워크 연결 오류: $error');
-      }
-
-      retryCount++;
-      await Future.delayed(Duration(seconds: 1)); // 1초 후에 재시도
-    }
-
-    print('재시도 횟수 초과'); // 최대 재시도 횟수를 초과하면 에러 메시지 출력
-  }
 
   Future<void> _getuserInfo() async {
     final response = await http.get(
@@ -191,30 +99,24 @@ class _GScoreAdminRegistState extends State<GScoreAdminRegist> {
       },
     );
     if (response.statusCode == 200) {
-      final maxScoreTemp = jsonDecode(response.body);
-      for (var item in maxScoreTemp) {
-        String username = item['name'];
-        int userid = item['student_id'];
-        userInfo[userid] = username;
+      final userInfoTemp = jsonDecode(response.body);
+      for (var item in userInfoTemp) {
+        String userName = item['name'];
+        int userId = item['student_id'];
+        int userGrade = item['grade'];
+        userInfo[userId] = userName;
+        //userInfo2[userId] = userInfo2[userId] = {userName: userGrade};
       }
+      //print(userInfo2);
     } else {
       throw Exception('예외 발생');
     }
   }
 
   //학생정보 리스트
-  Map<int, String> userInfo = {
-    20180621: "김민구",
-    20180620: "곽예빈",
-    20180619: "이나훈",
-    20170621: "김민구2",
-    20170620: "곽예빈2",
-    20170619: "이나훈2",
-    20160621: "김민구3",
-    20160620: "곽예빈3",
-    20160619: "이나훈3",
-    20180618: "박태수"
-  };
+  Map<int, String> userInfo = { };
+
+  //Map<int,Map<String,int>> userInfo2 = {};
 
   //선택한 학생 정보 저장
   Map<int, String> userInfosave = {};
@@ -237,23 +139,16 @@ class _GScoreAdminRegistState extends State<GScoreAdminRegist> {
     print(_activityType);
     print(_activityName);
     print(_score);
+    print(userInfosave.keys);
   }
 
   bool isEditable = false;
 
-  // 신청 상태에 대한 드롭다운형식의 콤보박스에서 선택된 값
-  String _applicationStatus = '대기';
 
   //비고란
   TextEditingController _contentController = TextEditingController();
 
-  // 반려 사유를 입력할 수 있는 텍스트 입력박스에서 입력된 값
-  String? _rejectionReason;
 
-  //파일이 저장값
-  PlatformFile? selectedFile;
-
-  int fileCheck = 0;
 
   //작성된 게시글 번호
   int postId = 0;
@@ -261,11 +156,7 @@ class _GScoreAdminRegistState extends State<GScoreAdminRegist> {
   //게시글이 정상적으로 업로드 되었는지 체크
   int postUploadCheck = 0;
 
-  //파일이 정상적으로 서버에 업로드 되었는지 체크
-  int fileUploadCheck = 0;
 
-  //업로드한 파일의 정보
-  Map<String, dynamic> fileInfo = {};
 
   final _formKey = GlobalKey<FormState>();
 
@@ -366,7 +257,7 @@ class _GScoreAdminRegistState extends State<GScoreAdminRegist> {
                                 child: InkWell(
                                   onTap: () {
                                     if (_searchId != null &&
-                                        !userInfosave!
+                                        !userInfosave
                                             .containsKey(_userid.text)) {
                                       int userid = int.parse(_userid.text);
                                       setState(() {
@@ -475,9 +366,6 @@ class _GScoreAdminRegistState extends State<GScoreAdminRegist> {
                     child: MaterialButton(
                       onPressed: () async {
                         await _writePostAndFile();
-                        if (fileUploadCheck == 1) {
-                          await _uploadfileToDB();
-                        }
                         if (postUploadCheck == 1) {
                           Navigator.of(context).pop();
                         } else {
